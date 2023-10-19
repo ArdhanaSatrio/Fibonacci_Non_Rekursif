24878b1144af905bf24659a405663451775974e2
ArdhanaSatrio <147833067+ArdhanaSatrio@users.noreply.github.com>
2023-10-19 10:14:24 +0700

    Add files via upload

---
+++ Fibonaci Non Rekursif.py
@@ -0,0 +1,14 @@
+#Fibonacci Non Rekursif Menggunakan List
+print("====Fibonnaci_Non_Rekursif")
+panjang = int(input('Masukkan panjang deret: '))
+
+fibo = [0, 1]
+
+for i in range(2, panjang):
+  angka1 = fibo[i - 2]
+  angka2 = fibo[i - 1]
+  angkaSelanjutnya = angka1 + angka2
+
+  fibo.append(angkaSelanjutnya)
+  
+print(fibo)
